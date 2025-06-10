c76cabed8851064b426255ff6b2dc10cb784d349
Killian.Le <killian.le@avepoint.com>
2025-06-10 15:24:35 +0700

    Create post form component

---
+++ src/components/create-post-form/CreatePostForm.tsx
@@ -0,0 +1,120 @@
+/** @format */
+
+import { Popover, Tooltip } from "antd";
+import React from "react";
+import ReactDOM from "react-dom";
+import { IoMdClose } from "react-icons/io";
+import { Button } from "../common";
+import { IoChevronDown } from "react-icons/io5";
+import { classNames } from "@/utils";
+import { IoCheckmarkCircleSharp } from "react-icons/io5";
+
+type IShareWith = "Everyone" | "Friends" | "Only Me";
+
+export const shareWithOptions: IShareWith[] = ["Everyone", "Friends", "Only Me"];
+
+const CreatePostForm: React.FunctionComponent = () => {
+    const [isOpen, setIsOpen] = React.useState(false);
+    const [isOpenShareWith, setIsOpenShareWith] = React.useState(false);
+    const [shareWith, setShareWith] = React.useState<IShareWith>("Everyone");
+    const [tempShareWith, setTempShareWith] = React.useState<IShareWith>("Everyone");
+
+    const handleOpen = () => {
+        setIsOpen(true);
+    };
+
+    const handleOverlayClick = (e: React.MouseEvent<HTMLDivElement>) => {
+        if (e.target === e.currentTarget) {
+            setIsOpen(false);
+        }
+    };
+
+    const onOpenChangeShareWithMenu = (isOpen: boolean) => {
+        setIsOpenShareWith(isOpen);
+        if (!isOpen) {
+            setShareWith(tempShareWith);
+        }
+    }
+
+    const handleChangeTempShareWith = (value: IShareWith) => {
+        setTempShareWith(value);
+    };
+
+    const shareWithContent = React.useMemo(() => {
+        return (
+            <div className="w-60 bg-white dark:bg-[#334155] normal-box-shadow !p-2 text-sm font-medium">
+                {shareWithOptions.map((option) => (
+                    <div
+                        className="!py-2 !px-3 flex items-center justify-between cursor-pointer bg-transparent hover:bg-gray-100 dark:hover:bg-gray-600 rounded-lg"
+                        onClick={() => handleChangeTempShareWith(option)}
+                        key={option}
+                    >
+                        <p className="flex-1 text-[#333] dark:text-white">{option}</p>
+                        {tempShareWith === option && <IoCheckmarkCircleSharp className="w-5 h-5 text-blue-500" />}
+                    </div>
+                ))}
+            </div>
+        );
+    }, [tempShareWith, isOpenShareWith]);
+
+    return (
+        <div className="!bg-white !rounded-xl normal-box-shadow md:!p-4 !p-2 !space-y-4 text-sm font-medium dark:!bg-[#1e293b]">
+            <div className="flex items-center md:!gap-3 gap-1">
+                <div className="flex-1 bg-slate-100 dark:bg-[#2f3c50] hover:!opacity-80 transition-all rounded-lg cursor-pointer" onClick={handleOpen}>
+                    <div className="!py-2.5 text-center text-[#4b5563] dark:text-white" tabIndex={0}>
+                        What do you have in mind?
+                    </div>
+                </div>
+                <Tooltip placement="top" title="New post with image" mouseEnterDelay={0.35}>
+                    <div
+                        className="cursor-pointer hover:opacity-80 !p-1 !px-1.5 rounded-lg transition-all bg-pink-100/60 hover:bg-pink-100 dark:bg-white/10 dark:hover:bg-white/20"
+                        tabIndex={0}
+                        onClick={handleOpen}
+                    >
+                        <img
+                            className="w-8 h-8 stroke-pink-600 fill-pink-200/70"
+                            src="https://img.icons8.com/external-smashingstocks-flat-smashing-stocks/66/external-Picture-technology-and-vr-smashingstocks-flat-smashing-stocks.png"
+                            alt="image-icon"
+                        />
+                    </div>
+                </Tooltip>
+                <Tooltip placement="top" title="New post with video" mouseEnterDelay={0.35}>
+                    <div
+                        className="cursor-pointer hover:opacity-80 !p-1 !px-1.5 rounded-lg transition-all bg-sky-100/60 hover:bg-sky-100 dark:bg-white/10 dark:hover:bg-white/20"
+                        tabIndex={0}
+                        onClick={handleOpen}
+                    >
+                        <img className="w-8 h-8 stroke-sky-600 fill-sky-200/70" src="https://img.icons8.com/plasticine/100/video-call.png" alt="video-icon" />
+                    </div>
+                </Tooltip>
+            </div>
+
+            {isOpen &&
+                ReactDOM.createPortal(
+                    <div className="fixed inset-0 backdrop-blur-xs bg-black/50 flex items-center justify-center z-50" onClick={handleOverlayClick}>
+                        <div className="bg-white dark:bg-[#1e293b] !shadow-lg rounded-md w-11/12 max-w-[560px] min-h-90 relative flex flex-col dialog-fade-in">
+                            <div className="flex items-center justify-between !py-3 !px-4 !border-b !border-gray-200">
+                                <div className="flex-1 text-center text-sm">Create status</div>
+                                <div className="!bg-transparent dark:hover:!bg-gray-500 hover:!bg-gray-200  transition-all cursor-pointer !p-1 rounded-full" onClick={() => setIsOpen(false)}>
+                                    <IoMdClose className="w-6 h-6 text-[#333] dark:text-white" />
+                                </div>
+                            </div>
+                            <div className="flex-1">Hello</div>
+                            <div className="flex items-center justify-between !py-4 !px-4">
+                                <Popover trigger="click" placement="topLeft" arrow={false} open={isOpenShareWith} onOpenChange={onOpenChangeShareWithMenu} content={shareWithContent}>
+                                    <div className="bg-[#f1f5f9] dark:bg-[#334155] w-28 !px-3 !py-1.5 rounded-2xl flex items-center cursor-pointer">
+                                        <div className="text-sm text-[#333] dark:text-white flex-1 !mr-1">{shareWith}</div>
+                                        <IoChevronDown className={classNames("w-4 h-4 text-[#333] dark:text-white transition-transform duration-300", { "rotate-180": isOpenShareWith })} />
+                                    </div>
+                                </Popover>
+                                <Button className="w-34 !h-9" displayText="Create" />
+                            </div>
+                        </div>
+                    </div>,
+                    document.getElementById("create-post-dialog-root")!
+                )}
+        </div>
+    );
+};
+
+export { CreatePostForm };
